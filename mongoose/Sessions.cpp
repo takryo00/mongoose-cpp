cb7cfd8a0d0b6c487f931e6c9dab97200b684377
ryo <takano@cloud-ace.jp>
2020-02-25 20:42:51 +0900

    fix

--- mongoose/Sessions.cpp
+++ mongoose/Sessions.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 static char charset[] = "abcdeghijklmnpqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-#define CHARSET_SIZE ((sizeof(charset)/sizeof(char)) - 1);
+#define CHARSET_SIZE ((sizeof(charset)/sizeof(char)) - 1)
 
 namespace Mongoose
 {
